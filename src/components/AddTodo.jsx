66eeb1fbfa243254b03bc692602318828816f4d5
Kavya Mittal <mister1729x@gmail.com>
2025-01-18 14:59:45 +0530

    added delete and add functionality not update

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,34 @@
+import React,{useState} from 'react'
+import {useDispatch} from 'react-redux'
+import {addTodo} from '../features/todo/todoSlice'
+const AddTodo = () => {
+
+    const[input, setInput] = useState('')
+    const dispatch = useDispatch()
+
+    const addTodoHandler = (e) => {
+        e.preventDefault()
+        dispatch(addTodo(input))
+        setInput('')
+    }
+
+    return (
+        <div>
+            <form onSubmit={addTodoHandler} className='space-x-3 mt-12'>
+                <input
+                    type='text'
+                    className='bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-white px-3 py-1'
+                    placeholder='Enter a Todo'
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <button
+                    type='submit'
+                    className='text-white bg-indigo-500 px-3 py-1 rounded hover:bg-indigo-600'
+                > Add Todo</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddTodo
